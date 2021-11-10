9c05cff43ccd7e5b3f715c8298cb994c32f2e06f
mateusz7812 <mateusz78122@gmail.com>
2021-11-10 21:21:09 +0100

    tests updated

--- zad1/Tests/LinearFunctionTest.cs
+++ zad1/Tests/LinearFunctionTest.cs
@@ -9,26 +9,37 @@ namespace Tests
         [Fact]
         public void TestCalc()
         {
-            var (x1, x2) = Program.Calc(0, 2, 1);
+            var (x1, x2, number) = Program.Calc(0, 2, 1);
             Assert.Equal(-0.5, x1);
             Assert.Null(x2);
+            Assert.Equal(1, number);
         }
 
         [Fact]
         public void TestCalc2()
         {
-            var (x1, x2) = Program.Calc(0, 3, 3);
+            var (x1, x2, number) = Program.Calc(0, 3, 3);
             Assert.Equal(-1, x1);
             Assert.Null(x2);
+            Assert.Equal(1, number);
         }
 
         [Fact]
         public void TestCalcBEqual0()
         {
-            var (x1, x2) = Program.Calc(0, 0, 3);
+            var (x1, x2, number) = Program.Calc(0, 0, 3);
             Assert.Null(x1);
             Assert.Null(x2);
+            Assert.Equal(0, number);
         }
 
+        [Fact]
+        public void TestCalcABCEqual0()
+        {
+            var (x1, x2, number) = Program.Calc(0, 0, 0);
+            Assert.Null(x1);
+            Assert.Null(x2);
+            Assert.Null(number);
+        }
     }
 }
--- zad1/Tests/SquareFunctionTest.cs
+++ zad1/Tests/SquareFunctionTest.cs
@@ -8,25 +8,28 @@ namespace Tests
         [Fact]
         public void TestCalcNoResults()
         {
-            var (x1, x2) = Program.Calc(1.1, 0, 3.3);
+            var (x1, x2, number) = Program.Calc(1.1, 0, 3.3);
             Assert.Null(x1);
             Assert.Null(x2);
+            Assert.Equal(0, number);
         }
         
         [Fact]
         public void TestCalcOneResult()
         {
-            var (x1, x2) = Program.Calc(1.1, 0, 0);
+            var (x1, x2, number) = Program.Calc(1.1, 0, 0);
             Assert.Equal(0, x1);
             Assert.Null(x2);
+            Assert.Equal(1, number);
         }
 
         [Fact]
         public void TestCalcTwoResults()
         {
-            var (x1, x2) = Program.Calc(1, 0, -1);
+            var (x1, x2, number) = Program.Calc(1, 0, -1);
             Assert.Equal(-1, x1);
             Assert.Equal(1, x2);
+            Assert.Equal(2, number);
         }
     }
 }
